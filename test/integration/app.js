61533f17a7438a5e7e5b7cd539e4c7094a125571
moiseshilario <moiseshilariu@gmail.com>
2017-08-22 16:29:57 -0300

    Create integration test

---
+++ test/integration/app.js
@@ -0,0 +1,27 @@
+describe('Routes Books', () => {
+  const Books = app.datasource.models.Books
+  const defaultBook = {
+    id: 1,
+    name: 'Default Book'
+  }
+
+  beforeEach(done => {
+    Books
+      .destroy({where: {}})
+      .then(() => Books.create(defaultBook))
+      .then(() => done())
+  })
+
+  describe('Route GET /books', () => {
+    it('should return a list of books', done => {
+      request
+        .get('/books')
+        .end((err, res) => {
+          expect(res.body[0].id).to.be.eql(defaultBook.id)
+          expect(res.body[0].name).to.be.eql(defaultBook.name)
+
+          done(err)
+        })
+    })
+  })
+})
