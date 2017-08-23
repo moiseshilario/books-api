205c4958bb4dc71588354e1334cdf68699d89f97
moiseshilario <moiseshilariu@gmail.com>
2017-08-23 15:08:02 -0300

    Create base for unit tests

---
+++ test/unit/controllers/books.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Controller: Books', () => {
+  describe('Get all books: getAll()', () => {
+    it('should return a list of books', () => {
+
+    })
+  })
+})
